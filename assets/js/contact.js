44dd7075735cbbc8b372fa22931af00f1a8269c9
chano <martin.britez@gmail.com>
2020-09-04 16:06:07 -0300

    add mas mejoras

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -67,6 +67,7 @@ $(document).ready(function(){
                             $('#success').fadeIn()
                             $('.modal').modal('hide');
 		                	$('#success').modal('show');
+                            $("#contactForm")[0].reset();
                         })
                     },
                     error: function() {
